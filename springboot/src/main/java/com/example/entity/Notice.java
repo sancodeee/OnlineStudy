c885cf15ae9b4ffeee04cf92181d4af9a4571b2c
sancodeee <wangsen715@gmail.com>
2024-01-15 00:28:09 +0800

    脚手架基本搭建

--- springboot/src/main/java/com/example/entity/Notice.java
+++ springboot/src/main/java/com/example/entity/Notice.java
@@ -1,62 +1,42 @@
 package com.example.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
 /**
  * 公告信息表
-*/
+ *
+ * @author wangsen
+ * @date 2024/01/15
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Notice implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    /** ID */
+    /**
+     * ID
+     */
     private Integer id;
-    /** 标题 */
+    /**
+     * 标题
+     */
     private String title;
-    /** 内容 */
+    /**
+     * 内容
+     */
     private String content;
-    /** 创建时间 */
+    /**
+     * 创建时间
+     */
     private String time;
-    /** 创建人 */
+    /**
+     * 创建人
+     */
     private String user;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
-    }
-
-    public String getUser() {
-        return user;
-    }
-
-    public void setUser(String user) {
-        this.user = user;
-    }
 
 }
